1f6bb08e6fcda35231c4aa2d5dae0c0bade4871d
Reid McCulloch <wrm7914@stern.nyu.edu>
2022-07-12 19:53:48 -0400

    User Choice

---
+++ game.py
@@ -0,0 +1,32 @@
+# this is the "game.py" file...
+
+print("Rock, Paper, Scissors, Shoot!")
+
+print("WELCOME TO ROCK PAPER SCSSORS GAME")
+
+print(f"You chose: '{user_choice}'")
+print(f"You chose: '{user_choice}'")
+
+# USE INPUTS
+
+user_choice = input("Please make a selection ('rock', 'paper', 'scissors'): ")
+
+
+
+# VALIDATE USER INPUTS
+
+# DETERMINE THE WINNER
+
+# DISPLAY RESULTS
+
+# Welcome 'Player One' to my Rock-Paper-Scissors game...
+# -------------------
+# Please choose either 'rock', 'paper', or 'scissors': rock
+# You chose: 'rock'
+print("You chose:", user_choice)
+# The computer chose: 'paper'
+# -------------------
+# Oh, the computer won. It's ok.
+# -------------------
+# Thanks for playing. Please play again!
+

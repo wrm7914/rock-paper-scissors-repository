3ab5a12daa95d05869273116c3c777ed0c6439f9
Reid McCulloch <wrm7914@stern.nyu.edu>
2022-07-12 20:46:49 -0400

    winner determination

--- game.py
+++ game.py
@@ -36,6 +36,28 @@ print("comuter chose:", computer_choice)
 # The computer chose: 'paper'
 # -------------------
 # Oh, the computer won. It's ok.
-# -------------------
+
+
+# adapted from code shared in slack by Bonnie:
+# https://nyu-tech-2335.slack.com/archives/C5WPFSB52/p1657672686150239
+
+
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print("Rock crushes scissors. You win!")
+    else:
+        print("Paper covers rock. You lose.")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Paper covers rock. You win!")
+    else:
+        print("Scissors cuts paper. You lose.")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("Scissors cuts paper. You win!")
+    else:
+        print("Rock crushes scissors. You lose.")
 # Thanks for playing. Please play again!
 
